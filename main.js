0897cc3a09ec6a249192e75cfecf22c6fed5575e
Freja <frej400d@stud.kea.dk>
2022-04-06 13:29:37 +0200

    GOT URL DATA BITCH

--- main.js
+++ main.js
@@ -1,24 +1,72 @@
 import "./sass/style.scss";
-import "./sass/style.scss";
 
-const urlCalculater = "https://kea-alt-del.dk/websitecarbon/site/?url=";
+//const urlCalculater = "https://kea-alt-del.dk/websitecarbon/site/?url=";
+//const form = document.querySelector("form");
+
 //const ttvData = ttv.json;
+const form = document.querySelector("form");
+
+const Dataobj = {
+  green: "",
+  cleanerThan: "",
+};
 
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   console.log("Hej Calculater");
-  calculateUrl();
+
+  form.addEventListener("submit", getUrl);
 }
 
-function calculateUrl() {
-  const form = document.querySelector("form");
-  let url = form.elements.url.value;
+function getUrl(event) {
+  let inputUrl = form.url.value;
+  //console.log(inputUrl);
+  event.preventDefault();
+  prepareData(inputUrl);
+}
+
+function prepareData(inputUrl) {
+  const carbonApi = "https://kea-alt-del.dk/websitecarbon/site/?url=";
+
+  let fullUrl = carbonApi.concat(inputUrl);
+  console.log(fullUrl);
+  loadJSON(fullUrl);
+}
+
+async function loadJSON(fullUrl) {
+  const response = await fetch(fullUrl);
+  const jsonData = await response.json();
+  console.log(jsonData);
+}
+
+// GET API
+/* 
+function calculateUrl(url) {
   console.log(url);
-  let result = urlCalculater.concat(url);
+
+  const carbonApi = "https://kea-alt-del.dk/websitecarbon/site/?url=";
+
+  const response = await fetch(carbonApi + url);
+  const data = await response.json();
+  
+}
+
+
+
+
+
+
+
+ function calculateUrl() {
+  const form = document.querySelector("form");
+  //let inputUrl = form.elements.url.value;
+  console.log(url);
+  let result = urlCalculater.concat("https://www.twitch.tv/");
   //urlCalculater +
   //`https://kea-alt-del.dk/websitecarbon/site/?url=https://${url}`;
   //console.log(result);
 
   console.log(result);
-}
+} 
+ */
---
+++ scripts/data.js
@@ -0,0 +1,25 @@
+/* const Dataobj = {
+  grams_co2: "",
+  green: "",
+  cleanerThan: "",
+};
+
+async function loadJSON() {
+  const respBad = await fetch("ttv.json");
+  const badData = await respBad.json();
+
+  const respGood = await fetch("portfolio.json");
+  const goodData = await respGood.json();
+
+  prepareData(badData, goodData);
+}
+
+function prepareData(badData, goodData) {
+const badDataPro = Object.create(Dataobj)
+
+badDataPro.grams_co2 = badData.grams
+badDataPro.cleanerThan = badData.cleanerThan;
+badDataPro.green = badData.green;
+
+}
+ */
